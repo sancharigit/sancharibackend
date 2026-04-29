cd4d5f239b2b185acafa777e9d387c6c256c6630
Shubhamdubey03 <shubham.dubeyargos@gmail.com>
2026-04-29 19:58:17 +0530

    changes

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -399,7 +399,8 @@ export const whatsappLogin = async (req, res) => {
 
 
         // Generate 6-digit OTP
-        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        // const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        const otp = "123456";
         const otpExpires = new Date(Date.now() + 10 * 60 * 1000); // 10 mins
 
         user.otp = otp;
