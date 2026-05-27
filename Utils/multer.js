c21f8ee47ebab20438b0f2f27a5808361bc99de7
Shubhamdubey03 <shubham.dubeyargos@gmail.com>
2026-05-27 15:34:45 +0530

    changes

--- Utils/multer.js
+++ Utils/multer.js
@@ -3,8 +3,8 @@ import path from 'path';
 import fs from 'fs';
 
 // Ensure uploads directory exists
-const uploadDir = 'uploads';
-// const uploadDir = path.resolve('/var/www/sanchari/uploads')
+// const uploadDir = 'uploads';
+const uploadDir = path.resolve('/var/www/sanchari/uploads')
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
